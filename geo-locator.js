3ba573ec14f386344c7db1b6072b3ee4021e341b
Ian Streator <istreator@yahoo.com>
2022-02-24 02:43:01 -0600

    refined/organized code for readability

--- geo-locator.js
+++ geo-locator.js
@@ -1,10 +1,7 @@
-// let location = {};
 async function getCoordinates() {
   const pos = await new Promise((res, rej) => {
     navigator.geolocation.getCurrentPosition(res, rej);
   });
-  //   location.lat = pos.coords.latitude;
-  //   location.lon = pos.coords.longitude;
   const location = {
     lat: pos.coords.latitude,
     lon: pos.coords.longitude,
